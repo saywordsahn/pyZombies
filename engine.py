9e4d9697f10aab224c927a92faf7dd9005fe032e
TCC - Admin <saywordsahn@gmail.com>
2021-12-28 13:12:47 +0900

    Output player to screen

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 import pygame
+from player import Player
+
 
 class Engine:
 
@@ -10,10 +12,15 @@ class Engine:
 
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.FULLSCREEN)
 
+        self.player = Player()
+        self.player_group = pygame.sprite.Group()
+        self.player_group.add(self.player)
+
     def run(self):
 
         while True:
 
+            # Input
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -23,3 +30,14 @@ class Engine:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         exit(0)
+
+            # Update
+
+            # Draw
+            self.screen.fill(pygame.color.Color('Black'))
+
+            self.player_group.draw(self.screen)
+
+            pygame.display.update()
+
+
--- player.py
+++ player.py
@@ -1,10 +1,16 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
-        self.START_SPEED = 200
-        self.START_HEALTH = 100
+        pygame.sprite.Sprite.__init__(self)
+        self.start_speed = 200
+        self.start_health = 100
+        self.health = self.start_health
+        self.max_health = self.start_health
+
         self.image = pygame.image.load('graphics/player.png')
-
-
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
